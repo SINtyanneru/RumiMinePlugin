7fa4b2a9b827c0358a855b55db7d697b58b39f7f
るみさん <irjitgd@gmail.com>
2023-06-19 22:31:21 +0900

    なぜか動かんけど、設定ファイルの生成機能

--- src/main/java/com/rumisystem/rumimineplugin/Config.java
+++ src/main/java/com/rumisystem/rumimineplugin/Config.java
@@ -77,14 +77,31 @@ public class Config{
 
     private static void Discord_ConfLoad(){
         try {
-            System.out.println(Paths.get(Config_Path + "/Discord.json").toFile());
+            File CFILE = new File(Config_Path + "/Discord.json");
+            if(CFILE.exists()){
+                System.out.println(Paths.get(Config_Path + "/Discord.json").toFile());
 
-            ObjectMapper  objectMapper = new ObjectMapper();
-            JsonNode json = objectMapper.readTree(Paths.get(Config_Path + "/Discord.json").toFile());
-            System.out.println("[ OK ]LoadConfig Discord:" + json);
-            System.out.println("[ *** ]Setting Config...");
+                ObjectMapper  objectMapper = new ObjectMapper();
+                JsonNode json = objectMapper.readTree(Paths.get(Config_Path + "/Discord.json").toFile());
+                System.out.println("[ OK ]LoadConfig Discord:" + json);
+                System.out.println("[ *** ]Setting Config...");
 
-            BOT_TOKEN = json.get("TOKEN").textValue();//トークン
+                BOT_TOKEN = json.get("TOKEN").textValue();//トークン
+            }else{
+                boolean created = CFILE.createNewFile();
+                if (created) {
+                    System.out.println("Created config file");
+                    FileWriter writer = new FileWriter(CFILE);
+                    writer.write("{\n" +
+                            "\t\"TOKEN\":\"BOT TOKEN HERE\"\n" +
+                            "}");
+                    System.out.println("Writed config file");
+
+                    main();
+                } else {
+                    System.out.println("ファイルの作成に失敗しました。");
+                }
+            }
         } catch(IOException ex) {
             ex.printStackTrace();
         }
@@ -92,19 +109,43 @@ public class Config{
 
     private static void msg_ConfLoad(){
         try {
-            ObjectMapper  objectMapper = new ObjectMapper();
-            JsonNode json = objectMapper.readTree(Paths.get(Config_Path + "/msg.json").toFile());
-            System.out.println("[ OK ]LoadConfig MSG:" + json);
-            System.out.println("[ *** ]Setting Config...");
+            File CFILE = new File(Config_Path + "/msg.json");
+            if(CFILE.exists()){
+                ObjectMapper  objectMapper = new ObjectMapper();
+                JsonNode json = objectMapper.readTree(Paths.get(Config_Path + "/msg.json").toFile());
+                System.out.println("[ OK ]LoadConfig MSG:" + json);
+                System.out.println("[ *** ]Setting Config...");
 
-            CONFIG_MSG_PROXYSTART = json.get("PROXY_START").textValue();//起動メッセージ
-            CONFIG_MSG_PROXYEXIT = json.get("PROXY_EXIT").textValue();//終了メッセージ
-            CONFIG_MSG_SERVERLOGIN = json.get("SERVER_LOGIN").textValue();//ログインメッセージ
-            CONFIG_MSG_SERVERLEAVE = json.get("SERVER_LEAVE").textValue();//ログアウトメッセージ
-            CONFIG_MSG_SERVERSWITCH = json.get("SERVER_SWITCH").textValue();//移動メッセージ
-            CONFIG_MSG_COMMAND_SERVERLIST = json.get("COMMAND_SERVER_LIST").textValue();
-            CONFIG_MSG_COMMAND_CUSTOMCHAT = json.get("CUSTOM_CHAT").textValue();
-            CONFIG_MSG_COMMAND_CUSTOMCHATBLOCK = json.get("CUSTOM_CHAT_BLOCK").textValue();
+                CONFIG_MSG_PROXYSTART = json.get("PROXY_START").textValue();//起動メッセージ
+                CONFIG_MSG_PROXYEXIT = json.get("PROXY_EXIT").textValue();//終了メッセージ
+                CONFIG_MSG_SERVERLOGIN = json.get("SERVER_LOGIN").textValue();//ログインメッセージ
+                CONFIG_MSG_SERVERLEAVE = json.get("SERVER_LEAVE").textValue();//ログアウトメッセージ
+                CONFIG_MSG_SERVERSWITCH = json.get("SERVER_SWITCH").textValue();//移動メッセージ
+                CONFIG_MSG_COMMAND_SERVERLIST = json.get("COMMAND_SERVER_LIST").textValue();
+                CONFIG_MSG_COMMAND_CUSTOMCHAT = json.get("CUSTOM_CHAT").textValue();
+                CONFIG_MSG_COMMAND_CUSTOMCHATBLOCK = json.get("CUSTOM_CHAT_BLOCK").textValue();
+            }else{
+                boolean created = CFILE.createNewFile();
+                if (created) {
+                    System.out.println("Created config file");
+                    FileWriter writer = new FileWriter(CFILE);
+                    writer.write("{\n" +
+                            "\t\"PROXY_START\":\":ballot_box_with_check: プロキシサーバーが起動しました！\",\n" +
+                            "\t\"PROXY_EXIT\":\":no_entry_sign: プロキシサーバーが停止しました\",\n" +
+                            "\t\"SERVER_LOGIN\":\"「** %1$s **」さんがログインしました！\",\n" +
+                            "\t\"SERVER_LEAVE\":\"「** %1$s **」さんが鯖に退出しました！\",\n" +
+                            "\t\"SERVER_SWITCH\":\":twisted_rightwards_arrows: 「** %1$s **」さんが「** %2$s **」に移動しました\",\n" +
+                            "\t\"COMMAND_SERVER_LIST\":\"現在の人数は、「%1$s」人です！\",\n" +
+                            "\t\"CUSTOM_CHAT\":\"<%1$s>%2$s\",\n" +
+                            "\t\"CUSTOM_CHAT_BLOCK\":\"その文字列は禁止されています！\"\n" +
+                            "}");
+                    System.out.println("Writed config file");
+
+                    main();
+                } else {
+                    System.out.println("ファイルの作成に失敗しました。");
+                }
+            }
         } catch(IOException ex) {
             ex.printStackTrace();
         }
@@ -112,14 +153,33 @@ public class Config{
 
     private static void config_ConfLoad(){
         try {
-            ObjectMapper  objectMapper = new ObjectMapper();
-            JsonNode json = objectMapper.readTree(Paths.get(Config_Path + "/config.json").toFile());
-            System.out.println("[ OK ]LoadConfig:" + json);
-            System.out.println("[ *** ]Setting Config...");
+            File CFILE = new File(Config_Path + "/config.json");
+            if(CFILE.exists()){
+                ObjectMapper  objectMapper = new ObjectMapper();
+                JsonNode json = objectMapper.readTree(Paths.get(Config_Path + "/config.json").toFile());
+                System.out.println("[ OK ]LoadConfig:" + json);
+                System.out.println("[ *** ]Setting Config...");
 
-            CONFIG_FUNCTION_DISCORD = Boolean.valueOf(json.get("DiscordBOT").textValue());//機能の有効無効化
-            CONFIG_FUNCTION_HUBCOM = Boolean.valueOf(json.get("HubCom").textValue());//機能の有効無効化
-            CONFIG_FUNCTION_CUSTOMCHAT = Boolean.valueOf(json.get("CustomChat").textValue());//機能の有効無効化
+                CONFIG_FUNCTION_DISCORD = Boolean.valueOf(json.get("DiscordBOT").textValue());//機能の有効無効化
+                CONFIG_FUNCTION_HUBCOM = Boolean.valueOf(json.get("HubCom").textValue());//機能の有効無効化
+                CONFIG_FUNCTION_CUSTOMCHAT = Boolean.valueOf(json.get("CustomChat").textValue());//機能の有効無効化
+            }else{
+                boolean created = CFILE.createNewFile();
+                if (created) {
+                    System.out.println("Created config file");
+                    FileWriter writer = new FileWriter(CFILE);
+                    writer.write("{\n" +
+                            "\t\"DiscordBOT\":\"false\",\n" +
+                            "\t\"HubCom\":\"false\",\n" +
+                            "\t\"CustomChat\":\"false\"\n" +
+                            "}");
+                    System.out.println("Writed config file");
+
+                    main();
+                } else {
+                    System.out.println("ファイルの作成に失敗しました。");
+                }
+            }
         } catch(IOException ex) {
             ex.printStackTrace();
         }
@@ -127,13 +187,31 @@ public class Config{
 
     private static void hubcom_ConfLoad(){
         try {
-            ObjectMapper  objectMapper = new ObjectMapper();
-            JsonNode json = objectMapper.readTree(Paths.get(Config_Path + "/hubcom.json").toFile());
-            System.out.println("[ OK ]LoadConfig HubCom:" + json);
-            System.out.println("[ *** ]Setting Config...");
+            File CFILE = new File(Config_Path + "/hubcom.json");
+            if(CFILE.exists()){
+                ObjectMapper  objectMapper = new ObjectMapper();
+                JsonNode json = objectMapper.readTree(Paths.get(Config_Path + "/hubcom.json").toFile());
+                System.out.println("[ OK ]LoadConfig HubCom:" + json);
+                System.out.println("[ *** ]Setting Config...");
 
-            CONFIG_HUBCOM_HUB = json.get("HUB").textValue();//HUb鯖を指定
-            CONFIG_HUBCOM_MSG = json.get("MSG").textValue();//メッセージを設定
+                CONFIG_HUBCOM_HUB = json.get("HUB").textValue();//HUb鯖を指定
+                CONFIG_HUBCOM_MSG = json.get("MSG").textValue();//メッセージを設定
+            }else{
+                boolean created = CFILE.createNewFile();
+                if (created) {
+                    System.out.println("Created config file");
+                    FileWriter writer = new FileWriter(CFILE);
+                    writer.write("{\n" +
+                            "\t\"HUB\":\"lobby\",\n" +
+                            "\t\"MSG\":\"NONE\"\n" +
+                            "}");
+                    System.out.println("Writed config file");
+
+                    main();
+                } else {
+                    System.out.println("ファイルの作成に失敗しました。");
+                }
+            }
         } catch(IOException ex) {
             ex.printStackTrace();
         }
@@ -144,21 +222,41 @@ public class Config{
 
         List<Replace_ChatDATA> replace_chatDATA = null;
         try {
-            StringBuilder builder = new StringBuilder();
+            File CFILE = new File(Config_Path + "/replace_chat.json");
+            if(CFILE.exists()){
+                StringBuilder builder = new StringBuilder();
 
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(Config_Path + "/replace_chat.json"), "UTF-8"))) {
-                String string = reader.readLine();
-                while (string != null){
-                    builder.append(string + System.getProperty("line.separator"));
-                    string = reader.readLine();
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(Config_Path + "/replace_chat.json"), "UTF-8"))) {
+                    String string = reader.readLine();
+                    while (string != null){
+                        builder.append(string + System.getProperty("line.separator"));
+                        string = reader.readLine();
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            replace_chatDATA = objectMapper.readValue(builder.toString(), new TypeReference<List<Replace_ChatDATA>>() {});
+                replace_chatDATA = objectMapper.readValue(builder.toString(), new TypeReference<List<Replace_ChatDATA>>() {});
 
-            CONFIG_CUSTOMCHAT_REPLASECHAT = replace_chatDATA;
+                CONFIG_CUSTOMCHAT_REPLASECHAT = replace_chatDATA;
+            }else{
+                boolean created = CFILE.createNewFile();
+                if (created) {
+                    System.out.println("Created config file");
+                    FileWriter writer = new FileWriter(CFILE);
+                    writer.write("[{\n" +
+                            "\t\"OLD\":\"置き換える前の内容\",\n" +
+                            "\t\"NEW\":\"置き換える先の内容\"\n" +
+                            "}]");
+                    System.out.println("Writed config file");
+
+                    main();
+                } else {
+                    System.out.println("ファイルの作成に失敗しました。");
+                }
+            }
         } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
@@ -168,21 +266,40 @@ public class Config{
 
         List<Block_ChatDATA> block_chatDATA = null;
         try {
-            StringBuilder builder = new StringBuilder();
+            File CFILE = new File(Config_Path + "/block_chat.json");
+            if(CFILE.exists()){
+                StringBuilder builder = new StringBuilder();
 
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get(Config_Path + "/block_chat.json")), "UTF-8"))) {
-                String string = reader.readLine();
-                while (string != null){
-                    builder.append(string + System.getProperty("line.separator"));
-                    string = reader.readLine();
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get(Config_Path + "/block_chat.json")), "UTF-8"))) {
+                    String string = reader.readLine();
+                    while (string != null){
+                        builder.append(string + System.getProperty("line.separator"));
+                        string = reader.readLine();
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            block_chatDATA = objectMapper.readValue(builder.toString(), new TypeReference<List<Block_ChatDATA>>() {});
+                block_chatDATA = objectMapper.readValue(builder.toString(), new TypeReference<List<Block_ChatDATA>>() {});
 
-            CONFIG_CUSTOMCHAT_BLOCKCHAT = block_chatDATA;
+                CONFIG_CUSTOMCHAT_BLOCKCHAT = block_chatDATA;
+            }else{
+                boolean created = CFILE.createNewFile();
+                if (created) {
+                    System.out.println("Created config file");
+                    FileWriter writer = new FileWriter(CFILE);
+                    writer.write("[{\n" +
+                            "\t\"WORD\":\"下ネタとか\"\n" +
+                            "}]");
+                    System.out.println("Writed config file");
+
+                    main();
+                } else {
+                    System.out.println("ファイルの作成に失敗しました。");
+                }
+            }
         } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
